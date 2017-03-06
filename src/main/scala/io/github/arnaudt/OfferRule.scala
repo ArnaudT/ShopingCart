7a794813cecf110dcd13900a170866d7b8a2e5ab
Arnaud Tanguy <arnaudtanguy@gmail.com>
2017-03-06 21:33:17 +0000

    Implementation of step 2 : Simple offers

--- src/main/scala/io/github/arnaudt/OfferRule.scala
+++ src/main/scala/io/github/arnaudt/OfferRule.scala
@@ -8,9 +8,26 @@ trait OfferRule {
 }
 
 class OneForOneOfferApple extends OfferRule {
-  def calculatePrice(products: Seq[Product]): PriceInCents = ???
+  def calculatePrice(products: Seq[Product]): PriceInCents = {
+    val filteredProducts = products.collect { case a: Apple => a }
+    if (filteredProducts.size % 2 == 0) {
+      PriceInCents(filteredProducts.map(_.priceInCents.value).sum / 2)
+    } else {
+      val otherProducts = filteredProducts.take(1)
+      PriceInCents(filteredProducts.drop(1).map(_.priceInCents.value).sum / 2 + otherProducts.map(_.priceInCents.value).sum)
+    }
+  }
 }
 
 class ThreeForTwoRuleOrange extends OfferRule {
-  def calculatePrice(products: Seq[Product]): PriceInCents = ???
+  def calculatePrice(products: Seq[Product]): PriceInCents = {
+    val filteredProducts = products.collect { case a: Orange => a }
+    val modulo = filteredProducts.size % 3
+    if (modulo == 0) {
+      PriceInCents(filteredProducts.map(_.priceInCents.value).sum * 2 / 3)
+    } else {
+      val otherProducts = filteredProducts.take(modulo)
+      PriceInCents(filteredProducts.drop(modulo).map(_.priceInCents.value).sum * 2 / 3 + otherProducts.map(_.priceInCents.value).sum)
+    }
+  }
 }
